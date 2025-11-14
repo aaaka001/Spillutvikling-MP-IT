67245a4fb21faecd1d9815769fc6a6c1f2748ff0
aaaka001 <aaaka001@osloskolen.no>
2025-11-14 13:09:21 +0100

    Init commit

---
+++ minigameScenes/character_body_2d.gd
@@ -0,0 +1,35 @@
+extends CharacterBody2D
+
+@export var speed = 400
+@export var jump_velocity: float = -300.0
+@export var gravity: float = 800.0
+var held_gifts: int = 0
+var catch_area: Area2D
+
+
+func get_input():
+	var input_direction = Input.get_vector("left", "right", "ui_up", "ui_down").x
+	velocity.x = input_direction * speed
+
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = jump_velocity
+		
+func _ready():
+	catch_area = get_node("Sprite/CatchArea")
+	catch_area.connect("area_entered", Callable(self, "_on_catch_area_entered"))
+	
+func _on_catch_area_entered(area: Area2D) -> void:
+	print("Area entered by:", area.get_parent().name)
+
+	if area.get_parent().name == "Gift":
+		held_gifts += 1
+		area.queue_free()  # remove the gift
+		print("Caught a gift! Total:", held_gifts)
+
+
+func _physics_process(delta):
+	if not is_on_floor():
+		velocity.y += gravity * delta
+
+	get_input()
+	move_and_slide()
---
+++ minigameScenes/spawner.gd
@@ -0,0 +1,25 @@
+extends Node2D
+
+@export var gift_scene: PackedScene   # drag your Gift.tscn here
+@export var spawn_interval: float = 1.0
+
+var spawn_timer: float = 0.0
+var screen_width: float = 1024.0  # adjust to your viewport width
+
+func _ready():
+	spawn_timer = spawn_interval
+	screen_width = get_viewport_rect().size.x
+
+func _process(delta: float) -> void:
+	spawn_timer -= delta
+	if spawn_timer <= 0:
+		spawn_gift()
+		spawn_timer = spawn_interval
+
+func spawn_gift() -> void:
+	if not gift_scene:
+		return
+	var gift = gift_scene.instantiate()
+	var x = randf_range(0, screen_width)
+	gift.position = Vector2(x, -50)
+	get_parent().add_child(gift)  # add gift to same scene as player
