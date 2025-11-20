ff969da64bf350798d95519e912b1d87bd4d6b73
Mari <180284303+Mahoa077@users.noreply.github.com>
2025-11-20 10:53:37 +0100

    torsdag før lunsj

---
+++ scenes/FourthMinigame/snøballene.gd
@@ -0,0 +1,8 @@
+extends RigidBody2D
+var speed = 800
+	
+func _ready():
+	look_at(get_global_mouse_position())
+
+func _physics_process(delta):
+	linear_velocity = Vector2.RIGHT.rotated(rotation) * speed
--- scenes/FourthMinigame/snøballkaster.gd
+++ scenes/FourthMinigame/snøballkaster.gd
@@ -1,5 +1,7 @@
 extends Node2D
 
+var Snøballene = preload("res://scenes/FourthMinigame/Snøballene.tscn")
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -9,3 +11,9 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	look_at(get_global_mouse_position())
+	if Input.is_action_just_pressed("ui_accept"):
+		var instance = Snøballene.instantiate()
+		instance.global_position = Vector2(global_position)
+		get_tree().current_scene.add_child(instance)
+		look_at(get_global_mouse_position())
+	
