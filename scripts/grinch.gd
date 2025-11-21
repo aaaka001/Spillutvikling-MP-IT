fca479bb7c931eec8263d1ba64eaaa5f6ec00b25
Diona <diona1mulaj@gmail.com>
2025-11-21 09:30:07 +0100

    s

--- scripts/grinch.gd
+++ scripts/grinch.gd
@@ -1,54 +1,45 @@
 extends CharacterBody2D
 
-const SPEED = 140
-var player
-
-func _ready():
-	player = get_parent().get_node("Player")
-
-func _physics_process(delta):
-	if player:
-		# Bare følg spilleren veldig enkelt
-		var dir = (player.global_position - global_position).normalized()
-		velocity = dir * SPEED
-		move_and_slide()
-
-extends CharacterBody2D
-
-@export var speed := 120.0
+@export var speed := 140.0
 @export var jump_force := -350.0
+@export var gravity := 900.0
+@export var obstacle_check_distance := 20.0
 
 var player
 
 func _ready():
 	player = get_parent().get_node("Player")
 
-func _physics_process(delta):
 
+func _physics_process(delta):
 	if not player:
 		return
 
-	# Alltid løp mot spilleren
-	if player.position.x > position.x:
+	# Horizontal chasing
+	if player.global_position.x > global_position.x:
 		velocity.x = speed
 	else:
 		velocity.x = -speed
 
-	# Hvis hindring foran → hopp
+	# Jump if obstacle ahead
 	if is_on_floor() and _obstacle_in_front():
 		velocity.y = jump_force
 
 	# Gravity
-	velocity.y += 900 * delta
+	velocity.y += gravity * delta
 
 	move_and_slide()
 
+
 func _obstacle_in_front() -> bool:
-	# Raycast fremover for å oppdage hindring
 	var space_state = get_world_2d().direct_space_state
 	var from = global_position
-	var to = global_position + Vector2(sign(velocity.x) * 20, 0)
+	var to = global_position + Vector2(sign(velocity.x) * obstacle_check_distance, 0)
 
-	var result = space_state.intersect_ray(from, to, [self])
-
+	var params = {
+		"from": from,
+		"to": to,
+		"exclude": [self]
+	}
+	var result = space_state.intersect_ray(params)
 	return result.size() > 0
