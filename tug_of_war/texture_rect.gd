6780c706e65c72f02ec9b554ca55abbf6b7dcc3a
Armankayas <armaniahhwastaken@gmail.com>
2025-11-25 06:45:01 +0100

    fiksa jukebox

---
+++ tug_of_war/texture_rect.gd
@@ -0,0 +1,9 @@
+extends TextureRect
+var juke_box = AudioStreamPlayer.new()
+
+func playing_music():
+	add_child(juke_box)
+	juke_box.stream = preload("res://tug_of_war/tug_music.mp3")
+	juke_box.bus = "Master"
+	juke_box.play()
+	
--- tug_of_war/tug.gd
+++ tug_of_war/tug.gd
@@ -4,19 +4,16 @@ var win_detected = preload("res://scenes/screens/PlayerWin/p1win.tscn")
 var clicksp1 = 0
 var clicksp2 = 0
 var winner_name = "none"
-@onready var juke_box: AudioStreamPlayer
 
 
 func _ready() -> void:
-	juke_box = AudioStreamPlayer.new()
-	add_child(juke_box)
-	juke_box.stream = preload("res://tug_of_war/tug_music.mp3")
-	juke_box.bus = "Master"
-	juke_box.autoplay = true
-	juke_box.play()
+	jukebox.playing_music()
+		
+
 	
 
-
+	
+	
 func _process(delta: float) -> void:
 	if Input.is_action_just_released("p1pull"):
 		position.x -= 15
@@ -28,7 +25,8 @@ func _process(delta: float) -> void:
 
 
 func _on_area_2d_2_area_entered(area: Area2D) -> void:
-	get_tree().change_scene_to_packed(win_detected)
+	jukebox.juke_box.stop()
+	jukebox.juke_box.queue_free()  
 	
 	if area.name == "p2":
 		winner_name = "Player 1"
@@ -37,3 +35,7 @@ func _on_area_2d_2_area_entered(area: Area2D) -> void:
 	if area.name == "p1":
 		winner_name = "Player 2"
 		print("p2 win")
+	get_tree().change_scene_to_packed(win_detected)
+		
+		
+		
