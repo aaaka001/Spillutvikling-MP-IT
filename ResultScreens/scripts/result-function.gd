a8a0159b0bf6699633a540e18375e3726cacfe6a
Diona <diona1mulaj@gmail.com>
2025-11-26 20:50:04 +0100

    victory &defeat screen completed
    graphics

--- ResultScreens/scripts/result-function.gd
+++ ResultScreens/scripts/result-function.gd
@@ -1,17 +1,29 @@
 extends Control
 
+@onready var menu_button = $"Menu"
+@onready var exit_button = $"Exit"
+@onready var try_again_button = $Redo
+
 func _ready():
-	$VBoxContainer/MenuButton.pressed.connect(_on_menu_pressed)
-	$VBoxContainer/QuitButton.pressed.connect(_on_quit_pressed)
+	if menu_button == null:
+		push_error("Menu button not found!")
+		return
+	if exit_button == null:
+		push_error("Exit button not found!")
+		return
+	if try_again_button == null:
+		push_error("Redo button not found!")
+		return
 
-
-func show_screen():
-	visible = true
-
+	menu_button.pressed.connect(_on_menu_pressed)
+	exit_button.pressed.connect(_on_quit_pressed)
+	try_again_button.pressed.connect(_on_redo_pressed)
 
 func _on_menu_pressed():
-	get_tree().change_scene_to_file("res://scenes/menu_screen.tscn")
-
+	get_tree().change_scene_to_file("res://scenes/screens/menu_screen.tscn")
 
 func _on_quit_pressed():
 	get_tree().quit()
+
+func _on_redo_pressed():
+	get_tree().change_scene_to_file("res://GrinchParkourMG/scenes/ParkourLevel.tscn")
