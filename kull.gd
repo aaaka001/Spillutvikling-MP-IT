463d7cba687c68ee6b67a07052ed199d35cc0e32
Mari <180284303+Mahoa077@users.noreply.github.com>
2025-11-27 09:07:03 +0100

    start screen og eskalere kamera

--- kull.gd
+++ kull.gd
@@ -8,3 +8,6 @@ func _process(delta: float) -> void:
 func _on_timer_timeout() -> void:
 	position.y = 0
 	$Timer.start()
+
+func _on_body_entered():
+	pass
--- scenes/FourthMinigame/snøballene.gd
+++ scenes/FourthMinigame/snøballene.gd
@@ -11,3 +11,7 @@ func _physics_process(delta):
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	body.queue_free()
 	queue_free()
+
+
+func _on_area_2d_area_entered(area: Area2D) -> void:
+	pass
