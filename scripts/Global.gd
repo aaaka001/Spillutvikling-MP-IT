512ce236c5a0d380c7244691988c2a36eab6a1d9
aaaka001 <aaaka001@osloskolen.no>
2025-11-28 10:27:34 +0100

    comit

---
+++ scripts/Global.gd
@@ -0,0 +1,19 @@
+extends Node
+
+var score: int = 0
+var score_label: Label = null
+var gift_speed_multiplier: float = 1.0
+var player_speed_multiplier: float = 1.0
+
+
+func register_score_label(label: Label):
+	score_label = label
+	update_score()
+
+func add_score(amount: int):
+	score += amount
+	update_score()
+
+func update_score():
+	if score_label:
+		score_label.text = "Score: " + str(score)
